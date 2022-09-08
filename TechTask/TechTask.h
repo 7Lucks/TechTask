ad4039b9943e9e6105eecbf1255f37c8cae85941
7Luck <straenger@gmail.com>
2022-09-08 21:05:18 +0300

    Initial Commit

---
+++ TechTask/TechTask.h
@@ -0,0 +1,18 @@
+//
+//  TechTask.h
+//  TechTask
+//
+//  Created by Дмитрий Игнатьев on 08.09.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TechTask.
+FOUNDATION_EXPORT double TechTaskVersionNumber;
+
+//! Project version string for TechTask.
+FOUNDATION_EXPORT const unsigned char TechTaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TechTask/PublicHeader.h>
+
+
